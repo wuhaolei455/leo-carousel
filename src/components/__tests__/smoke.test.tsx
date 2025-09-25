e82791981a1d725bc43d1c8d83914af59af012ef
wuhaolei <wuhaolei@kanyun.com>
2025-09-25 15:38:27 +0800

    fix: change named import to default import in test file

--- src/components/__tests__/smoke.test.tsx
+++ src/components/__tests__/smoke.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Carousel } from '../Carousel';
+import Carousel from '../Carousel';
 
 describe('Carousel', () => {
   it('renders without crashing', () => {
